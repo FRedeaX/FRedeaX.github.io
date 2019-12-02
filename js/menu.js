195ec83555fe4f8f908a1645a97be449cfe86758
Timofey <FRedeaX@Yandex.ru>
2019-12-03 01:02:09 +0500

    ИсправленияИсправление аккордиона в блоке меню

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,39 @@
-const menuToggleAll = document.querySelectorAll('.menu-accordion__item'),
+const menuToggleAll = document.querySelectorAll('.menu-accordion__btn'),
+menuAccordionContent = document.querySelector('.menu-accordion__content'),
       menuToggleActive = 'menu-accordion__item--active';
-let lastMenuActive = 1;
+let lastMenuActive = 1,
+    menuWidth = document.querySelector('.menu').offsetWidth,
+    // fontSize = parseInt(window.getComputedStyle(document.querySelector('html')).getPropertyValue('font-size')),
+    allToggleWidth = menuToggleAll[0].offsetWidth * menuToggleAll.length;
       
-menuToggleAll.forEach((menuToggle, menuToggleIndex) => {
+menuToggleAll.forEach((menuToggle, menuToggleIndex) => {  
   menuToggle.addEventListener('click', ()=> {
-    if (menuToggle.classList.contains(menuToggleActive)) {  
-      menuToggle.classList.toggle(menuToggleActive);
+    menuItem = menuToggle.parentNode;
+    // если true, то нажатие произошло по активной секции
+    if (menuItem.classList.contains(menuToggleActive)) {  
+      menuItem.classList.remove(menuToggleActive);
       lastMenuActive = null;     
-    } else {
-      if (lastMenuActive) menuToggleAll[lastMenuActive - 1].classList.toggle(menuToggleActive);       
-      menuToggle.classList.toggle(menuToggleActive);   
+    } else { 
+      if (menuWidth < (630 + allToggleWidth)) {
+        menuWidth -= allToggleWidth;
+        menuToggle.nextElementSibling.style.width = menuWidth+'px';
+      } 
+      if (lastMenuActive) menuToggleAll[lastMenuActive - 1].parentNode.classList.toggle(menuToggleActive);       
+      menuItem.classList.add(menuToggleActive);   
       lastMenuActive = menuToggleIndex + 1;
     }
-
-    menuCloseBtn = document.querySelector('.menu-accordion__btn');
-    menuCloseBtn.addEventListener('click', ()=>{
-      menuToggle.classList.toggle(menuToggleActive);   
-      lastMenuActive = menuToggleIndex + 1;
-    });
   });
 });
+
+const menuCloseBtnAll = document.querySelectorAll('.menu-accordion__close-btn');
+menuCloseBtnAll.forEach((menuCloseBtn, menuToggleIndex) => {  
+  menuCloseBtn.addEventListener('click', ()=> {
+    menuCloseBtn.parentNode.parentNode.classList.toggle(menuToggleActive);   
+    lastMenuActive = null;
+  });
+});
+
+window.addEventListener('resize', ()=>{
+  menuWidth = document.querySelector('.menu').offsetWidth;
+  allToggleWidth = menuToggleAll[0].offsetWidth * menuToggleAll.length
+})
