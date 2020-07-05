e2a8de15330b8078e34b883d5b60b5085c5b11fc
Timofey <FRedeaX@Yandex.ru>
2020-07-05 22:51:33 +0500

    Оптимизация 0.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,137 +1,156 @@
-const { src, dest, task, series, watch, parallel } = require('gulp');
-const rm = require('gulp-rm');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+const { src, dest, task, series, watch, parallel } = require("gulp");
+const rm = require("gulp-rm");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
 const reload = browserSync.reload;
-const sassGlob = require('gulp-sass-glob');
-const autoprefixer = require('gulp-autoprefixer');
-const px2rem = require('gulp-smile-px2rem');
-const gcmq = require('gulp-group-css-media-queries');
-const cleanCSS = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const svgo = require('gulp-svgo');
-const svgSprite = require('gulp-svg-sprite');
-const gulpif = require('gulp-if');
+const sassGlob = require("gulp-sass-glob");
+const autoprefixer = require("gulp-autoprefixer");
+const px2rem = require("gulp-smile-px2rem");
+const gcmq = require("gulp-group-css-media-queries");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify-es").default;
+const babel = require("gulp-babel");
+const svgo = require("gulp-svgo");
+const svgSprite = require("gulp-svg-sprite");
+const gulpif = require("gulp-if");
 
 const env = process.env.NODE_ENV;
-const { SRC_PATH, DIST_PATH, STYLE_LIBS } = require('./gulp.config');
+const { SRC_PATH, DIST_PATH, STYLE_LIBS } = require("./gulp.config");
 
-sass.compiler = require('node-sass');
+sass.compiler = require("node-sass");
 
 task("clean", () => {
-	return src(`${DIST_PATH}/**/*`, { read: false }).pipe(rm());
+  return src(`${DIST_PATH}/**/*`, { read: false }).pipe(rm());
 });
 
 task("copy:html", () => {
-	return src(`${SRC_PATH}/*.html`)
-		.pipe(dest(`${DIST_PATH}`))
-		.pipe(reload({ stream: true }));
+  return src(`${SRC_PATH}/*.html`)
+    .pipe(dest(`${DIST_PATH}`))
+    .pipe(reload({ stream: true }));
 });
 
 task("copy:font", () => {
-	return src(`${SRC_PATH}/fonts/*.*`)
-		.pipe(dest(`${DIST_PATH}/fonts`))
-		.pipe(reload({ stream: true }));
+  return src(`${SRC_PATH}/fonts/*.*`)
+    .pipe(dest(`${DIST_PATH}/fonts`))
+    .pipe(reload({ stream: true }));
 });
 
 task("copy:img", () => {
-	return src(`${SRC_PATH}/img/**/*.*`)
-		.pipe(dest(`${DIST_PATH}/img`))
-		.pipe(reload({ stream: true }));
+  return src(`${SRC_PATH}/img/**/*.*`)
+    .pipe(dest(`${DIST_PATH}/img`))
+    .pipe(reload({ stream: true }));
 });
 
 task("copy:video", () => {
-	return src(`${SRC_PATH}/video/*.*`)
-		.pipe(dest(`${DIST_PATH}/video`))
-		.pipe(reload({ stream: true }));
+  return src(`${SRC_PATH}/video/*.*`)
+    .pipe(dest(`${DIST_PATH}/video`))
+    .pipe(reload({ stream: true }));
 });
 
-task('styles', () => {
-	return src([...STYLE_LIBS])
-		.pipe(gulpif(env === 'dev', sourcemaps.init()))
-		.pipe(concat('main.scss'))
-		.pipe(sassGlob())
-		.pipe(sass().on('error', sass.logError))
-		// .pipe(px2rem())
-		.pipe(gulpif(env === 'prod', gcmq()))
-		.pipe(gulpif(env === 'prod', autoprefixer({ cascade: false })))
-		.pipe(gulpif(env === 'prod', cleanCSS()))
-		.pipe(gulpif(env === 'dev', sourcemaps.write()))
-		.pipe(dest(`./${DIST_PATH}`));
+task("styles", () => {
+  return (
+    src([...STYLE_LIBS])
+      .pipe(gulpif(env === "dev", sourcemaps.init()))
+      .pipe(concat("main.scss"))
+      .pipe(sassGlob())
+      .pipe(sass().on("error", sass.logError))
+      // .pipe(px2rem())
+      .pipe(gulpif(env === "prod", gcmq()))
+      .pipe(gulpif(env === "prod", autoprefixer({ cascade: false })))
+      .pipe(gulpif(env === "prod", cleanCSS()))
+      .pipe(gulpif(env === "dev", sourcemaps.write()))
+      .pipe(dest(`./${DIST_PATH}`))
+  );
 });
 
-task('script', () => {
-	return src(`${SRC_PATH}/js/*.js`)
-		.pipe(sourcemaps.init())
-		.pipe(concat('main.js', { newLine: ';' }))
-		// .pipe(uglify())
-		.pipe(sourcemaps.write())
-		.pipe(dest(`./${DIST_PATH}`));
-})
-
-task('icons', () => {
-	return src(`${SRC_PATH}/img/icons/*.svg`)
-		.pipe(svgo({
-			plugins: [{
-				removeAttrs: {
-					attrs: '(fill|stroke|style|width|height|data.*)'
-				}
-			}]
-		}))
-		.pipe(svgSprite({
-			mode: {
-				symbol: {
-					sprite: '../sprite.svg'
-				}
-			}
-		}))
-		.pipe(dest(`${DIST_PATH}/img/icons`))
+task("script", () => {
+  return (
+    src(`${SRC_PATH}/js/*.js`)
+      .pipe(gulpif(env === "dev", sourcemaps.init()))
+      .pipe(concat("main.js", { newLine: ";" }))
+      // .pipe(
+      //   babel({
+      //     presets: ["@babel/preset-env"],
+      //   })
+      // )
+      .pipe(gulpif(env === "prod", uglify()))
+      .pipe(gulpif(env === "dev", sourcemaps.write()))
+      .pipe(dest(`./${DIST_PATH}`))
+  );
 });
 
-task('browser-sync', () => {
-	browserSync.init({
-		server: {
-			baseDir: `./${DIST_PATH}`
-		},
-		open: false
-	});
+task("icons", () => {
+  return src(`${SRC_PATH}/img/icons/*.svg`)
+    .pipe(
+      svgo({
+        plugins: [
+          {
+            removeAttrs: {
+              attrs: "(fill|stroke|style|width|height|data.*)",
+            },
+          },
+        ],
+      })
+    )
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: "../sprite.svg",
+          },
+        },
+      })
+    )
+    .pipe(dest(`${DIST_PATH}/img/icons`));
 });
 
-task('watch', () => {
-	watch(`./${SRC_PATH}/css/**/*.scss`, series('styles'));
-	watch(`./${SRC_PATH}/*.html`, series('copy:html'));
-	watch(`./${SRC_PATH}/js/*.js`, series('script'));
-	watch(`./${SRC_PATH}/img/icons/*.svg`, series('icons'));
-})
+task("browser-sync", () => {
+  browserSync.init({
+    server: {
+      baseDir: `./${DIST_PATH}`,
+    },
+    open: false,
+  });
+});
 
-task('default', series(
-	'clean',
-	parallel(
-		'copy:html',
-		'copy:font',
-		'copy:img',
-		'copy:video',
-		'styles',
-		'script',
-		'icons'
-	),
-	parallel(
-		'watch',
-		'browser-sync'
-	)
-));
+task("watch", () => {
+  watch(`./${SRC_PATH}/css/**/*.scss`, series("styles"));
+  watch(`./${SRC_PATH}/*.html`, series("copy:html"));
+  watch(`./${SRC_PATH}/js/*.js`, series("script"));
+  watch(`./${SRC_PATH}/img/icons/*.svg`, series("icons"));
+});
 
-task('build', series(
-	'clean',
-	parallel(
-		'copy:html',
-		'copy:font',
-		'copy:img',
-		'copy:video',
-		'styles',
-		'script',
-		'icons'
-	)
-));
+task(
+  "default",
+  series(
+    "clean",
+    parallel(
+      "copy:html",
+      "copy:font",
+      "copy:img",
+      "copy:video",
+      "styles",
+      "script",
+      "icons"
+    ),
+    parallel("watch", "browser-sync")
+  )
+);
+
+task(
+  "build",
+  series(
+    "clean",
+    parallel(
+      "copy:html",
+      "copy:font",
+      "copy:img",
+      "copy:video",
+      "styles",
+      "script",
+      "icons"
+    )
+  )
+);
--- src/js/map.js
+++ src/js/map.js
@@ -1,35 +1,38 @@
 (function () {
-	ymaps.ready(init);
-	function init() {
-		let myMap = new ymaps.Map('map', {
-			center: [55.7486, 37.5986],
-			zoom: 14
-		});
+  window.onload = ymaps.ready(init);
+  function init() {
+    let map = new ymaps.Map("map", {
+      center: [55.7486, 37.5986],
+      zoom: 14,
+    });
 
-		const points = [
-			{
-				x: 55.7572,
-				y: 37.6158,
-				address: 'ул. Охотный ряд'
-			},
-			{
-				x: 55.7450,
-				y: 37.6008,
-				address: 'Гоголевский бульвар'
-			}
-		]
+    const points = [
+      {
+        x: 55.7572,
+        y: 37.6158,
+        address: "ул. Охотный ряд",
+      },
+      {
+        x: 55.745,
+        y: 37.6008,
+        address: "Гоголевский бульвар",
+      },
+    ];
 
-		points.forEach(point => {
-			myPlacemark = new ymaps.Placemark([point.x, point.y], {
-				balloonContent: point.address
-			}, {
-				iconLayout: 'default#image',
-				iconImageHref: 'img/map/choccoPoint.png',
-				hideIconOnBalloonOpen: false,
-				iconImageOffset: [-17, -40]
-			}),
-
-				myMap.geoObjects.add(myPlacemark);
-		});
-	}
-})()
+    points.forEach((point) => {
+      (myPlacemark = new ymaps.Placemark(
+        [point.x, point.y],
+        {
+          balloonContent: point.address,
+        },
+        {
+          iconLayout: "default#image",
+          iconImageHref: "img/map/choccoPoint.png",
+          hideIconOnBalloonOpen: false,
+          iconImageOffset: [-17, -40],
+        }
+      )),
+        map.geoObjects.add(myPlacemark);
+    });
+  }
+})();
